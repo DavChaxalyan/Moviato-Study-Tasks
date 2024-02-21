2319c5c236789771c67d5df8b7957061caa3bba1
Davit <dchaxalyan@gmail.com>
2024-02-21 20:51:33 +0400

    task7

---
+++ Task7/script.js
@@ -0,0 +1,6 @@
+// let p = document.querySelector('p');
+
+// function hideShow() {
+//     p.style.opacity = btn.innerText == 'Hide' ? 0 : 1;
+//     btn.innerText = btn.innerText == 'Show' ? 'Hide' : 'Show'
+// }
