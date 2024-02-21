4449ac102d8b4b0a5ed5ccfa06ea26afadaa34b1
Davit <dchaxalyan@gmail.com>
2024-02-21 23:34:27 +0400

    task12

---
+++ Task12/script.js
@@ -0,0 +1,21 @@
+// let arr = ['images/1.jpeg', 'images/2.jpeg', 'images/3.jpeg'];
+// let img = document.querySelector('img')
+// let count = 0;
+
+// function next() {
+//     if(count == arr.length - 1){
+//         count = 0
+//     } else {
+//         count++
+//     }
+//     img.src = arr[count]
+// }
+
+// function previou() {
+//     if(count == 0){
+//         count = arr.length-1
+//     } else {
+//         count--
+//     }
+//     img.src = arr[count]
+// }
