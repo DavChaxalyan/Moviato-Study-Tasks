2dc9164994d81df326d6257581297ee56e1fcf0d
Davit <dchaxalyan@gmail.com>
2024-02-21 22:42:19 +0400

    task11

---
+++ Task11/script.js
@@ -0,0 +1,10 @@
+// let inpName = document.querySelector('.name');
+// let email = document.querySelector('.email');
+
+// const validation = () => {
+//     if(inpName.value == ''){
+//         alert('enter your name');
+//     } else if(email.value == ''){
+//         alert('enter your email');
+//     };
+// };
