ee51f8a48d40b91a0f2f096ab7666eec04e1e478
Davit <dchaxalyan@gmail.com>
2024-02-21 22:10:51 +0400

    task10

---
+++ Task10/script.js
@@ -0,0 +1,18 @@
+// let list = document.querySelectorAll('li');
+
+// list.forEach(elm => {
+//     elm.addEventListener('mouseenter', function(){
+//         elm.style.color = randomColor()
+//     });
+//     elm.addEventListener('mouseout', function(){
+//         elm.style.color = 'rgb(240, 50, 50)';
+//     });
+// })
+
+// function randomColor() {
+//     let arr = [];
+//     for (let i = 0; i < 3; i++) {
+//         arr.push(Math.floor(Math.random() * 255))
+//     };
+//     return `rgb(${arr[0]}, ${arr[1]}, ${arr[2]})`
+// }
