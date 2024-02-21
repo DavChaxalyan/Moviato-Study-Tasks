93bbd5f8700c0c67ba9a2acc7e2d1ea67b93576e
Davit <dchaxalyan@gmail.com>
2024-02-21 21:40:48 +0400

    task9

---
+++ Task9/script.js
@@ -0,0 +1,8 @@
+// let input = document.querySelector('.inp');
+// let submit = document.querySelector('.sub');
+
+// submit.addEventListener('click', function() {
+//     if (input.value.length == 0) {
+//         alert('Please enter an email address')
+//     }
+// })
