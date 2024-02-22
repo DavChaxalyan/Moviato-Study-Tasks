9597574f7878af8590271071b9c8538ab05c1b55
Davit <dchaxalyan@gmail.com>
2024-02-22 20:31:21 +0400

    task13

---
+++ Task13/script.js
@@ -0,0 +1,22 @@
+// let p = document.querySelector('.p');
+// let arr = ['HELLO', 'HELP', 'SEARCH', 'PROFILE'];
+// let buttons = document.querySelectorAll('button');
+// console.log(buttons);
+
+// buttons.forEach(elm => {
+//     elm.addEventListener('click', changeText)
+// });
+
+// function changeText() {
+//     for (let i = 0; i < buttons.length; i++) {
+//         if (buttons[i].className == 'active') {
+//             buttons[i].classList.remove('active')
+//         }        
+//     };
+//     this.className = 'active';
+//     buttons.forEach((elm, i) => {
+//         if (elm.className == 'active') {
+//             p.innerText = arr[i];
+//         }
+//     })
+// }
