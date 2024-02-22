c03d8029bd22d85d07338431868a7425bac349f6
Davit <dchaxalyan@gmail.com>
2024-02-22 22:32:04 +0400

    task15

---
+++ Task15/script.js
@@ -0,0 +1,10 @@
+// let icon = document.querySelector('i');
+// let menu2 = document.querySelector('.menu2');
+
+// function menu() {
+//     if(menu2.style.display != 'none'){
+//         menu2.style.display = 'none'
+//     } else {
+//         menu2.style.display = 'block'
+//     }
+// }
