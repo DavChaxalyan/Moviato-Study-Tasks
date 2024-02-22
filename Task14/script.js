b57b3510c151b1761652ff25b50d93a7f2c8b4c2
Davit <dchaxalyan@gmail.com>
2024-02-22 21:12:28 +0400

    task14

---
+++ Task14/script.js
@@ -0,0 +1,14 @@
+// let namee = document.querySelector('.name');
+// let surname = document.querySelector('.surname');
+// let age = document.querySelector('.age');
+// let email = document.querySelector('.email');
+// let id = 1;
+
+// function save() {
+//     if (namee.value == '' || surname.value == '' || age.value == '' || email.value == '') {
+//         alert('error, enter all inputs')
+//     } else {
+//         let users = {name: namee.value, surname: surname.value, age: age.value, email: email.value}
+//         localStorage.setItem(id++, JSON.stringify(users))
+//     }
+// }
